59f17c87dcd102a4ac8e704256fe313c9da8c382
Elias Souza <eliasnsz.dev@gmail.com>
2025-05-06 17:07:30 -0300

    :wrench: chore: install `tailwind-css`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
+import { Geist } from "next/font/google";
 import { ReactNode } from "react";
+import "../styles/globals.css";
+import { Metadata } from "next";
+
+const geist = Geist({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Book Suggestions",
+  description: "Descubra novas leituras de acordo com o seu gosto!",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={geist.className}>
       <body>{children}</body>
     </html>
   );
